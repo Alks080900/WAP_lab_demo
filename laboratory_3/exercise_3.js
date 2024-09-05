930202ee23c27c68785cc6680bda2c4e36b7b167
AlksOblg <89616946+Alks080900@users.noreply.github.com>
2024-09-04 21:33:29 -0500

    Update exercise_3.js

--- laboratory_3/exercise_3.js
+++ laboratory_3/exercise_3.js
@@ -10,6 +10,6 @@ console.log(getNewArray(sampleArray));
 /*
 Lesson learned: its kinda similar to Java API streams, at first
 * I had the right code for it, but I got an error by using
-* contains() hahaha lol, but I now I know that the right
+* contains() hahaha lol, but now I know that the right
 function for it is incudes()
 */
